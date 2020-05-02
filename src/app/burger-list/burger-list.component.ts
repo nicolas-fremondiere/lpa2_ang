1d328035ccff29872ba80ad0a8545a7e39b9cfa3
Nicolas Fremondiere <n.fremondiere@outlook.fr>
2020-05-02 17:56:17 +0200

    Responsive UI and css improvment

--- src/app/burger-list/burger-list.component.ts
+++ src/app/burger-list/burger-list.component.ts
@@ -8,12 +8,13 @@ import { BurgersService } from '../api_burger/api/burgers.service';
   styleUrls: ['./burger-list.component.css']
 })
 export class BurgerListComponent implements OnInit {
-
+  breakingpoint
   burgerList;
   constructor(private burgerService : BurgersService) { }
 
   ngOnInit(): void {
     this.getBurgers();
+    this.breakingpoint = (window.innerWidth <=550) ? 2 : 6
   }
 
   getBurgers() :void{
@@ -21,4 +22,9 @@ this.burgerList =this.burgerService
 .listBurgers().subscribe(burgerList => this.burgerList = burgerList);
 
   }
+  onResize(event)
+   {
+     this.breakingpoint = (event.target.innerWidth <= 550) ? 2 : 6;
+   }
+
 }
